6de501a50c5a98183a367175b6cc2fd1a70c279a
Иван Зайцев <ivzaycev0717@yandex.ru>
2023-07-26 18:08:33 +0300

    start_of_developing

---
+++ settings.py
@@ -0,0 +1,9 @@
+# Главное окно игры
+TITLE = 'The Mystery of the Mansion'
+ICON = 'data/images/logo.ico'
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+# Режим редактора(конструктора) уровней
+TILE_SIZE = 64
+ANIMATION_SPEED = 8
