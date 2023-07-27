a391a9616b2abd8e1527e3bebba65b55243ae4bc
Иван Зайцев <ivzaycev0717@yandex.ru>
2023-07-27 20:26:04 +0300

    start_screen

--- settings.py
+++ settings.py
@@ -1,8 +1,19 @@
 # Главное окно игры
 TITLE = 'The Mystery of the Mansion'
-ICON = 'data/images/logo.ico'
+ICON_PATH = 'data/images/icons/logo.png'
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+
+# Данные автора
+GIT_HUB = 'github.com/IvanZaycev0717'
+E_MAIL = 'ivzaycev0717@yandex.ru'
+TELEGRAM = 'Telegram ID: 5984823073'
+AUTHOR_TIME = 10000
+
+# Шрифты
+FONT_PATH_1 = 'data/font/font1.ttf'
+FONT_PATH_2 = 'data/font/font2.ttf'
+FONT_PATH_3 = 'data/font/font3.ttf'
 
 # Режим редактора(конструктора) уровней
 TILE_SIZE = 64
